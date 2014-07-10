83c7c91783afea27d4e186a7b1895d62bcda6d74
6cker845 <nick_fun6king@yahoo.co.jp>
2014-07-10 17:02:52 +0900

    先生に質問

--- js/setting.js
+++ js/setting.js
@@ -1,7 +1,16 @@
+/*
 var todo = document.getElementById("#todo").value;
 
 function output(){
  var outputArea = document.getElementById("#outputArea").value;
  outputArea.innerHTML = todo + "したー？" ;
 }
+*/
 
+/*
+  function textout(){
+	  if (document.getElementById){
+		  document.getElementById("output").innerHTML=document.form1.text1.value;
+      }
+  };
+*/
