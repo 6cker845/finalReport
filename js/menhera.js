ccee03bf136642f13189a999683527c3c7fe7be9
6cker845 <nick_fun6king@yahoo.co.jp>
2014-07-11 20:54:19 +0900

    menhera.htmlにtableを追加。メンヘラコメントが出来るように。

--- js/menhera.js
+++ js/menhera.js
@@ -2,3 +2,19 @@ function output(){
  var outputArea = document.getElementById("#outputArea").value;
  outputArea.innerHTML = todo + "したー？" ;
 }
+
+var counter = 0;
+function AddTableRows(){
+	// カウンタを回す
+	counter++;
+
+	var table1 = document.getElementById("table1");
+	var row1 = table1.insertRow(counter);
+	var cell1 = row1.insertCell(0);
+
+	cell1.setAttribute("class","name");
+	cell1.className = 'name';
+	var HTML1 = 'あああ';
+	cell1.innerHTML = HTML1;
+
+}
