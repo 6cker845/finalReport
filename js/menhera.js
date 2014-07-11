92dd54c73f78a8236cce77dcac90fc1f14a71c3d
6cker845 <nick_fun6king@yahoo.co.jp>
2014-07-11 21:21:06 +0900

    メンヘラ発言リスト作成。

--- js/menhera.js
+++ js/menhera.js
@@ -3,6 +3,9 @@ function output(){
  outputArea.innerHTML = todo + "したー？" ;
 }
 
+var comments = new Array(
+		"", "今日はちゃんとやった？", "本当にやったのー？", "ねーこたえてよぉー", "今なにしてるの？", "なんで無視するの？", "もう無視されるの耐えられない", "なんかいってよ", "もう私ダメ", "信じてたのに", "リスカしよ。")
+
 var counter = 0;
 function AddTableRows(){
 	// カウンタを回す
@@ -11,10 +14,10 @@ function AddTableRows(){
 	var table1 = document.getElementById("table1");
 	var row1 = table1.insertRow(counter);
 	var cell1 = row1.insertCell(0);
-
 	cell1.setAttribute("class","name");
 	cell1.className = 'name';
-	var HTML1 = 'あああ';
+/* 	var HTML1 = 'あああ'; */
+	var HTML1 = comments[counter];
 	cell1.innerHTML = HTML1;
 
 }
