584cb9caa3bac03a621899f969e23ba280f7ca4f
6cker845 <nick_fun6king@yahoo.co.jp>
2014-07-12 00:31:46 +0900

    ボタンを押すと指定した秒数毎にメンヘラが発言する。

--- js/menhera.js
+++ js/menhera.js
@@ -7,24 +7,7 @@ function output(){
 var comments = new Array(
 		"", "今日はちゃんとやった？", "本当にやったのー？", "ねーこたえてよぉー", "今なにしてるの？", "なんで無視するの？", "もう無視されるの耐えられない", "なんかいってよ", "もう私ダメ", "信じてたのに", "リスカしよ。")
 
-var counter = 0;
-var cmtCount = 0;
-function AddTableRows(){
-	// カウンタを回す
-	counter++;
-	cmtCount++;
-
-	var table1 = document.getElementById("table1");
-	var row1 = table1.insertRow(counter);
-	var cell1 = row1.insertCell(0);
-	cell1.setAttribute("class","name");
-	cell1.className = 'name';
-/* 	var HTML1 = 'あああ'; */
-	var HTML1 = comments[cmtCount];
-	cell1.innerHTML = HTML1;
-
-}
-
+/* タイマー */
 
 var PassSec; // 秒数カウント用変数
 
@@ -35,11 +18,28 @@ function showPassage() {
    document.getElementById("PassageArea").innerHTML = msg; // 表示更新
 }
 
+//コメント更新
+var counter = 0;
+var cmtCount = 0;
+function showComment() { 
+	counter++;
+	cmtCount++;
+
+	var table1 = document.getElementById("table1");
+	var row1 = table1.insertRow(counter);
+	var cell1 = row1.insertCell(0);
+	cell1.setAttribute("class","name");
+	cell1.className = 'name';
+	var HTML1 = comments[cmtCount];
+	cell1.innerHTML = HTML1;
+}
+
 // 繰り返し処理の開始
 function startShowing() {
    PassSec = 0; // カウンタのリセット
    PassageID = setInterval('showPassage()',1000); // タイマーをセット(1000ms間隔)
-   document.getElementById("startcount").disabled = true; // 開始ボタンの無効化
+   cmtID = setInterval('showComment()', 3000);
+   document.getElementById("startcount").disabled = true; // 開始ボタンの無効化   
 }
 
 
