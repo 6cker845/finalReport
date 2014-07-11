4e5a44acd487fb17ac4061aed8c707421cb3634a
eikicks <eikicks@yhaoo.co.jp>
2014-07-11 23:57:23 +0900

    ふぁ

---
+++ menhera.js
@@ -0,0 +1,8 @@
+var todo = document.getElementById("#todo").value;
+
+function output(){
+ var outputArea = document.getElementById("#outputArea").value;
+outputArea.innerHTML = todo + "したー？" ;
+}
+
+initApp();
