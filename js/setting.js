a261d58481cfc1c99244e20dac4e7d9e4ebe6031
6cker845 <nick_fun6king@yahoo.co.jp>
2014-07-17 11:20:40 +0900

    数値が入ってないとエラーが出る事がうまく行かないので、元通りに戻した

--- js/setting.js
+++ js/setting.js
@@ -1,9 +1,10 @@
 var error = document.getElementById("error");
 var getTask = document.getElementById("todo");
-var getSeconds = document.form1.second.value;
+/* var getSeconds = document.form1.second.value; */
 
 var storage = localStorage;
 
+/*
 var isNumber = function(a){
 	return !Number.isNaN(a);
 };
@@ -15,6 +16,7 @@ var showError = function(){
 var hideError = function(){
 	error.setAttribute("class", "hidden");
 };
+*/
 
 function loadText(){
 	if (document.getElementById){
@@ -30,12 +32,11 @@ function textout(){
 	startTimer();
 }
 
+/*
 var isReady = function(a){
 	return isNumber(a);
 };
 	
-//各箱に値が入っていることを確認し、はいってたら画面遷移、はいってなかったらエラー
-//jsのみで画面遷移はできるっぽい。
 function checkDetails(){
 	var a = getSeconds.value;
 	hideError();
@@ -45,8 +46,6 @@ function checkDetails(){
 		loadText();
 		location.href="menhera.html";
 	}
-
-//授業でつかったコードを使用して、数値が代入されないと次に進まないようにする。
 }
 
 var initApp = function(){
@@ -56,6 +55,7 @@ var initApp = function(){
 };
 
 initApp();
+*/
 
 
 
