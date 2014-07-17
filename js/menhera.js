9aa9d4db8402f2c294f06405eb84d06daf5f5a24
Risatommy <risa555@gmail.com>
2014-07-17 18:16:46 +0900

    選択box
    選択boxのデザイン変更

--- js/menhera.js
+++ js/menhera.js
@@ -97,7 +97,7 @@ function displayCheck(){
 
 	if(answer){
 		showPassage();
-		var menhera = "目標設定から" + PassHour + "時間" + PassMin + "分" + PassSec + "秒たったよ!おめでとう！";
+		var menhera = "目標設定から" + PassHour + "時間" + PassMin + "分" + PassSec + "秒たったよ! <br> おめでとう！<br>";
 /* 		var menhera = day + "日目だね！おめでとう！"; */
         document.getElementById("reloadMH").innerHTML = menhera;
         cmtCount = 0;
