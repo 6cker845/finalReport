85bce286cb6b16d9dffe1383d53261c304559d5b
6cker845 <nick_fun6king@yahoo.co.jp>
2014-07-17 10:51:07 +0900

    数値を代入しないとエラーがでるようにしたい途中

--- js/setting.js
+++ js/setting.js
@@ -1,6 +1,6 @@
-var error = document.querySelector("#error");
-var getTask = document.querySelector("#todo");
-var getSeconds = document.querySelector("#secondID");
+var error = document.getElementById("error");
+var getTask = document.getElementById("todo");
+var getSeconds = document.form1.second.value;
 
 var storage = localStorage;
 
@@ -30,34 +30,32 @@ function textout(){
 	startTimer();
 }
 
-/*
-var isReady = function(operator, a, b){
-	return isDivision(operator, a, b) || isModulus(operator, a, b) ||
-	isMultiplication(operator, a, b) || isSubtraction(operator, a, b) || isAddition(operator, a, b);
+var isReady = function(a){
+	return isNumber(a);
 };
-*/
 	
 //各箱に値が入っていることを確認し、はいってたら画面遷移、はいってなかったらエラー
 //jsのみで画面遷移はできるっぽい。
 function checkDetails(){
-
+	var a = getSeconds.value;
 	hideError();
-	if(getTask.value != null || getSeconds.value != null){
+	if(!isReady(a)){
+		showError();
+	} else {
 		loadText();
 		location.href="menhera.html";
-	} else {
-		showError();
 	}
 
 //授業でつかったコードを使用して、数値が代入されないと次に進まないようにする。
 }
 
+var initApp = function(){
+	var checkButton = document.querySelector("#checkButton");
 
+	checkButton.addEventListener("click", checkDetails);
+};
 
-
-//登録ボタンでcheckDetailsを呼び出す。
-//もし時間とやる事に値が入っているのが確認できたら(if(値 != null))画面遷移させる
-//値が入っていなかったらエラー
+initApp();
 
 
 
