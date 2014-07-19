834417a2a34e5bd96c516f14873045c6fb30ab00
6cker845 <nick_fun6king@yahoo.co.jp>
2014-07-19 14:52:10 +0900

    プレゼン用に動くよう修正！

--- js/menhera.js
+++ js/menhera.js
@@ -11,7 +11,7 @@ var hours = Number(storage.getItem("hourInterval"));
 
 var inputHour = Number(storage.getItem("startHour"));
 var inputMinute = Number(storage.getItem("startMinute"));
-var inputOperator = storage.getItem("operator");
+/* var inputOperator = storage.getItem("operator"); */
 console.log(inputHour);
 console.log(inputMinute);
 
@@ -24,11 +24,13 @@ var alarm = {
 var menheraComments = new Array(
 		"今日はちゃんとやった？", "ねえ、今日はちゃんとやった？", "本当にやったのー？", "ねーこたえてよぉー", "どうせ他の女と連絡してるんでしょ？","私はあなたをこんなに思っているのに","私なんて、その程度だったんだね","ちょっと聞いてるの？","見てるんでしょ？","今なにしてるの？", "なんで無視するの？", "馬鹿にしてんの？","もう無視されるの耐えられない", "なんかいってよ", "最低の彼氏だね","つらぃ","聞いてよ、お願い。。","お願いだからさぁ","鬱になりそう","診察終るまでにはお願いね","死にたいって言ったらやってくれるの？","もう私ダメ","信じてたのに","マジ無理","アアアアァァァァァァ","カラダで許してくれる？","私は何をすればいいの…ねえ。教えてよ。","こんなに面倒くさくて気持ち悪くて性格が悪くて人にも好かれない私だけど、どうか嫌わないでください。愛してください。","死ねばいいんでしょ！！私がいなくなればいいんでしょ！！","もうリスカしよ。");
 
+/*
 var ikemenComments = new Array(
 		"今日はしっかりやったのか？","俺のこと考えすぎて、手がついてないだろ","早く終らして、出かけようぜ","何にも言わないと本気チューしちゃうぞ!","お前が心配だからだぞ。","おい、大丈夫か？","いつまでも待ってるからな。","会いたいんだから、早く終らしてくれよ。","おまえの好きってのはその程度か？","もぅﾏﾁﾞ無理。 ﾘｽｶしょ。。。");
 		
 var aneComments = new Array(
 		"今日はちゃんとやったのよね？", "ねえ、今日はちゃんとやったのよね？", "本当に、やったのよね？信じていいのよね？", "こらッ!答えなさいよ！", "どうせ他の女と連絡してるんじゃないの？","私はあなたのこと、こんなに思っているのに何様だと思っているの？","私なんて、その程度だったんだんだね。","ちょっと聞いてるのかしら？返事しなさいよ！","見てるんでるの、ばれてるわよ？","今なにしてるのかしら？答えられないのかしら？");
+*/
 
 
 /* タイマー */
@@ -62,14 +64,20 @@ function showPassage(){
 }
 
 function showAlarmMessage(){
-	var operator = inputOperator.value;
+/* 	var operator = inputOperator.value; */
 	cmtCount++
 	
+	var message = menheraComments[cmtCount];
+	
+/*
 	if(operator == "メンヘラ"){
 		var message = menheraComments[cmtCount];
 	} else if(operator == "年上お姉さん"){
 		var message = aneCommens[cmtCount];
-	} else if(operator == "イケメン風"){}
+	} else if(operator == "イケメン風"){
+		var message = ikemenComments[cmtCount];
+	}
+*/
 
 	if(Notification.permission == "granted"){
 		var notification = new Notification(message);
